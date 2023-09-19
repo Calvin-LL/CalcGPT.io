9fab2960069e7a3fad45b5223c134356cbc5b80a
Calvin Liang <me@calvin.sh>
2023-09-18 18:25:33 -0700

    feat: migrate model to babbage-002

--- netlify/functions/math.ts
+++ netlify/functions/math.ts
@@ -18,20 +18,22 @@ export const handler = stream(async (event) => {
       body: "math expression too long",
     };
   }
-  const temperature = parseFloat(event.queryStringParameters?.t ?? "NaN");
+  let temperature = parseFloat(event.queryStringParameters?.t ?? "NaN");
   if (isNaN(temperature) || !isFinite(temperature)) {
     return {
       statusCode: 400,
       body: "invalid temperature",
     };
   }
-  const topP = parseFloat(event.queryStringParameters?.p ?? "NaN");
+  temperature = Math.max(0, Math.min(2, temperature));
+  let topP = parseFloat(event.queryStringParameters?.p ?? "NaN");
   if (isNaN(topP) || !isFinite(topP)) {
     return {
       statusCode: 400,
       body: "invalid topP",
     };
   }
+  topP = Math.max(0, Math.min(1, topP));
 
   const prompt = `1+1=2\n5-2=3\n2*4=8\n9/3=3\n10/3=3.33333333333\n${math}=`;
   let stream: Awaited<ReturnType<OpenAI["completions"]["create"]>>;
@@ -39,7 +41,7 @@ export const handler = stream(async (event) => {
   try {
     const openAI = new OpenAI();
     stream = await openAI.completions.create({
-      model: "curie",
+      model: "babbage-002",
       temperature,
       top_p: topP,
       stop: "\n",
