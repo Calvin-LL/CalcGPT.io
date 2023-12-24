3593df70c4c4649dd081611aa163d13ce7afe60b
Calvin Liang <me@calvin.sh>
2023-12-24 02:41:52 -0800

    fix: fix math function
    fix: fix math function
    
    fix: fix math function
    
    fix: fix math function
    
    fix: fix math function
    
    fix: fix math function
    
    fix: fix math function
    
    fix: fix math function
    
    fix: fix math function
    
    fix: fix math function

--- netlify/functions/math.ts
+++ netlify/functions/math.ts
@@ -1,66 +1,65 @@
-import { stream, type StreamingResponse } from "@netlify/functions";
+import { type Context } from "@netlify/functions";
 import OpenAI from "openai";
 import { MATH_LENGTH_LIMIT } from "../../src/core/CalcGPT3";
 
-export const handler = stream(async (event) => {
-  console.log(event.queryStringParameters);
+export default async function (
+  req: Request,
+  context: Context,
+): Promise<Response> {
+  const searchParams = new URL(req.url).searchParams;
 
-  const math = event.queryStringParameters?.m;
+  console.log(Object.fromEntries(searchParams.entries()));
+
+  const math = searchParams.get("m");
   if (!math) {
-    return {
-      statusCode: 400,
-      body: "invalid query parameter",
-    };
+    return new Response("invalid query parameter", {
+      status: 400,
+    });
   }
   if (math.length > MATH_LENGTH_LIMIT) {
-    return {
-      statusCode: 400,
-      body: "math expression too long",
-    };
+    return new Response("math expression too long", {
+      status: 400,
+    });
   }
-  let temperature = parseFloat(event.queryStringParameters?.t ?? "NaN");
+  let temperature = parseFloat(searchParams.get("t") ?? "NaN");
   if (isNaN(temperature) || !isFinite(temperature)) {
-    return {
-      statusCode: 400,
-      body: "invalid temperature",
-    };
+    return new Response("invalid temperature", {
+      status: 400,
+    });
   }
   temperature = Math.max(0, Math.min(2, temperature));
-  let topP = parseFloat(event.queryStringParameters?.p ?? "NaN");
+  let topP = parseFloat(searchParams.get("p") ?? "NaN");
   if (isNaN(topP) || !isFinite(topP)) {
-    return {
-      statusCode: 400,
-      body: "invalid topP",
-    };
+    return new Response("invalid topP", {
+      status: 400,
+    });
   }
   topP = Math.max(0, Math.min(1, topP));
 
   const prompt = `1+1=2\n5-2=3\n2*4=8\n9/3=3\n10/3=3.33333333333\n${math}=`;
-  let stream: Awaited<ReturnType<OpenAI["completions"]["create"]>>;
+  let response: Response;
 
   try {
     const openAI = new OpenAI();
-    stream = await openAI.completions.create({
-      model: "babbage-002",
-      temperature,
-      top_p: topP,
-      stop: "\n",
-      prompt,
-      stream: true,
-    });
+    response = await openAI.completions
+      .create({
+        model: "babbage-002",
+        temperature,
+        top_p: topP,
+        stop: "\n",
+        prompt,
+        stream: true,
+      })
+      .asResponse();
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: "api error",
-    };
+    return new Response("api error", {
+      status: 500,
+    });
   }
 
-  return {
+  return new Response(response.body, {
     headers: {
       "content-type": "text/event-stream",
     },
-    statusCode: 200,
-    // @ts-expect-error accesses `response` which is a private property
-    body: stream.response.body as unknown as StreamingResponse["body"],
-  };
-});
+  });
+}
